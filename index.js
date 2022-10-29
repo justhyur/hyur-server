4036be831deb9825c8e1b7c0f8b4c1fb2c2486bd
Riccardo Scrizzi <riccardoscrizzi@gmail.com>
2022-10-29 20:06:16 -0100

    bcn assets working

--- index.js
+++ index.js
@@ -8,8 +8,36 @@ dotenv.config();
 
 //GLOBALS
 const PORT = process.env.PORT || 8080;
-const { BCN_USER, BCN_PSWD } = process.env;
-// const BROWSER = await puppeteer.launch();
+const { BCN_USER, BCN_PSWD, SERVER_TOKEN } = process.env;
+let BCN_ASSETS;
+
+//FUNCTIONS
+const getBCNAssets = async () => {
+    const browser = await puppeteer.launch();
+    console.log("Opening browser...");
+    const page = await browser.newPage();
+    await page.goto('https://particulares.bcn.cv/', {
+        waitUntil: 'networkidle0', 
+        timeout: 0
+    });
+
+    const frameHandle = await page.$("iframe[id=mainFrame]");
+    const frame = await frameHandle.contentFrame();
+
+    await frame.evaluate(({BCN_USER, BCN_PSWD}) => {
+        document.querySelector("#ctl00_ctl00_Utilizador").value = BCN_USER;
+        document.querySelector("#ctl00_ctl00_Password").value = BCN_PSWD;
+        document.querySelector("#ctl00_ctl00_SubmitBtn").click();
+    }, {BCN_USER, BCN_PSWD});
+
+    await frame.waitForNavigation({waitUntil: 'networkidle0',});
+    const assets = await frame.evaluate(() => {
+        return document.querySelector("#ctl00_ctl00_Ctrl_CPIN1_GridViewActivos > thead > tr > th.text-right").innerText;
+    });
+    await browser.close();
+    console.log("Browser closed.");
+    return assets;
+}
 
 app.use(express.json());
 
@@ -18,7 +46,24 @@ app.listen(PORT, () => {
 });
 
 app.get('/test', (req, res) => {
-    res.status(200).send({
-        print: BCN_USER,
-    });
+    res.status(200).send('API is online.');
 });
+
+app.get('/bcn-assets', (req, res) => {
+    const {token} = req.query;
+    if(token !== SERVER_TOKEN){
+        res.status(401).send('Token not valid.');
+    }
+    res.status(200).send(BCN_ASSETS);
+});
+
+//RUNTIME
+const runtime = async (seconds) => {
+    const milliseconds = seconds * 1000;
+    console.log('Runtiming...');
+    setTimeout(()=>{
+        runtime(milliseconds);
+    }, milliseconds);
+    BCN_ASSETS = await getBCNAssets();
+}
+runtime(5 * 60);
