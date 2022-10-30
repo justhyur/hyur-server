a8f9ebec00d9d55ec482fbe2fb34187f103a83b4
Riccardo Scrizzi <riccardoscrizzi@gmail.com>
2022-10-30 18:38:17 -0100

    updated bank api

--- cv-banks.js
+++ cv-banks.js
@@ -1,7 +1,10 @@
+const puppeteer = require('puppeteer');
 
-const getBCNAssets = async (browser, env) => {
-    if(!browser){return null}
-    const {BCN_USER, BCN_PSWD} = env;
+const getBCNAssets = async (userName, password) => {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox']
+    });
     const page = await browser.newPage();
     console.log("BCN Page opened.");
     await page.goto('https://particulares.bcn.cv/', {
@@ -12,44 +15,46 @@ const getBCNAssets = async (browser, env) => {
     const frameHandle = await page.$("iframe[id=mainFrame]");
     const frame = await frameHandle.contentFrame();
 
-    await frame.evaluate(({BCN_USER, BCN_PSWD}) => {
-        document.querySelector("#ctl00_ctl00_Utilizador").value = BCN_USER;
-        document.querySelector("#ctl00_ctl00_Password").value = BCN_PSWD;
+    await frame.evaluate(({userName, password}) => {
+        document.querySelector("#ctl00_ctl00_Utilizador").value = userName;
+        document.querySelector("#ctl00_ctl00_Password").value = password;
         document.querySelector("#ctl00_ctl00_SubmitBtn").click();
-    }, {BCN_USER, BCN_PSWD});
+    }, {userName, password});
 
     await frame.waitForNavigation({waitUntil: 'networkidle0',});
     const assets = await frame.evaluate(() => {
         return document.querySelector("#ctl00_ctl00_Ctrl_CPIN1_GridViewActivos > thead > tr > th.text-right").innerText;
     });
 
-    await page.close();
+    await browser.close();
     console.log("BCN Page closed.");
 
     return assets;
 }
 
-const getCAIXAAssets = async (browser, env) => {
-    if(!browser){return null}
-    const {CAIXA_USER, CAIXA_PSWD} = env;
+const getCAIXAAssets = async (userName, password) => {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox']
+    });
     const page = await browser.newPage();
     console.log("CAIXA Page opened.");
     await page.goto('https://caixanet.caixa.cv/', {
         waitUntil: 'networkidle0', 
         timeout: 0
     });
-    await page.evaluate(({CAIXA_USER, CAIXA_PSWD}) => {
-        document.querySelector("#username").value = CAIXA_USER;
-        document.querySelector("#password").value = CAIXA_PSWD;
+    await page.evaluate(({userName, password}) => {
+        document.querySelector("#username").value = userName;
+        document.querySelector("#password").value = password;
         document.querySelector("#login-form > div > div.col-lg-12.col-lg-offset-3 > div.row.mt-3.pt-1.justify-content-between > div.col-12.col-sm-6.order-0.order-sm-1 > button").click();
-    }, {CAIXA_USER, CAIXA_PSWD});
+    }, {userName, password});
 
     await page.waitForNavigation({waitUntil: 'networkidle0',});
     const assets = await page.evaluate(() => {
         return document.querySelector("#master-col > div > div > div:nth-child(1) > div > div > div.mb-4.pt-3.pb-4.border-bottom > div.col.text-right > span").innerText;
     });
 
-    await page.close();
+    await browser.close();
     console.log("CAIXA Page closed.");
 
     return assets;
--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 const dotenv = require('dotenv');
 const fs = require('fs');
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const {getBCNAssets, getCAIXAAssets} = require('./cv-banks');
-const puppeteer = require('puppeteer');
-let BROWSER;
 
 dotenv.config();
 
@@ -28,6 +27,7 @@ const readJSONFile = (path) => {
 }
 
 //API
+app.use(cors());
 app.use(express.json());
 
 app.get('/test', (req, res) => {
@@ -35,17 +35,24 @@ app.get('/test', (req, res) => {
 });
 
 app.get('/cv-assets/:bank', (req, res) => {
-    const {token} = req.query;
+    const {userName, password, token} = req.query;
     if(token !== SERVER_TOKEN){
         res.status(401).send('Token is missing or not valid.');
+    }else{
+        const {bank} = req.params;
+        const bankFunction = bank === 'bcn' ? getBCNAssets : getCAIXAAssets;
+        bankFunction(userName, password)
+        .then(assets => {
+            res.status(200).send(assets);
+        })
+        .catch(err => {
+            console.log(err);
+        })
     }
-    const {bank} = req.params;
-    const assets = readJSONFile(`./database/${bank}_assets.json`);
-    res.status(200).send(assets);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started on PORT ${PORT}.`)
+    console.log(`Server started on PORT ${PORT}.`);
 });
 
 //RUNTIME
@@ -55,20 +62,4 @@ const runtime = (minutes) => {
     setTimeout(()=>{
         runtime(milliseconds);
     }, milliseconds);
-    //BCN
-    getBCNAssets(BROWSER, process.env).then(bcnAssets => {
-        writeJSONFile(bcnAssets, './database/bcn_assets.json');
-    });
-    //CAIXA
-    getCAIXAAssets(BROWSER, process.env).then(caixaAssets => {
-        writeJSONFile(caixaAssets, './database/caixa_assets.json');
-    });
 }
-
-puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox']
-}).then(res => {
-    BROWSER = res;
-    runtime(process.env.RUNTIME_MINUTES);
-});
